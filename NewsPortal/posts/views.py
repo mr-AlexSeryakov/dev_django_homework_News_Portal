5a4830d2d8e7f7cb4789c61635291a7350d69685
Alex <aleksandrseryakov@MacBook-Air.local>
2025-10-01 20:19:47 +0300

    Создана страница news и подключена admin panel

--- NewsPortal/posts/views.py
+++ NewsPortal/posts/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
 
-# Create your views here.
+class PostList(ListView):
+    """Список всех новостей"""
+    model = Post
+    template_name = 'post_list.html'
+    context_object_name = 'posts'
+
+class PostDetail(DetailView):
+    """Отдельная новость"""
+    model = Post
+    template_name = 'post_detail.html'
+    context_object_name = 'post'
